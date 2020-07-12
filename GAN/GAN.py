98fd40da12b2963bb435d4eab0abdf821baf5f5c
Vitou Phy <phy_vitou@live.com>
2020-07-12 16:06:01 +0900

    try a different loss function (wgan)

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -8,11 +8,12 @@ from torch.utils.data.sampler import SubsetRandomSampler
 import pytorch_lightning as pl
 from pytorch_lightning.core.lightning import LightningModule
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-DIR_PATH = "/kaggle/input/stanford-dogs-dataset/images/Images"
+#DIR_PATH = "/kaggle/input/stanford-dogs-dataset/images/Images"
+DIR_PATH = "/data/vitou/100DaysofCode/datasets/stanford-dogs-dataset/images/Images"
+#DIR_PATH = "/data/vitou/100DaysofCode/datasets/mnistasjpg/trainingSet/trainingSet"
 
 class Discriminator(nn.Module):
     def __init__(self):
@@ -38,7 +39,7 @@ class Discriminator(nn.Module):
             
             nn.Linear(1024, 256),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 2)
+            nn.Linear(256, 1)
         )
         
     def forward(self, x):
@@ -97,16 +98,18 @@ class GAN(LightningModule):
         if optimizer_idx == 0:
             x, label = batch
             x, label = x.to(device), label.to(device)
-            label = torch.ones_like(label).to(device)
+            #label = torch.ones_like(label).to(device)
             y_hat_real = self.discriminator(x)
-            real_loss = F.cross_entropy(y_hat_real, label)
+            #real_loss = F.cross_entropy(y_hat_real, label)
             
-            z = torch.rand((x.size(0), 128)).to(device)
-            label = torch.zeros_like(label).to(device)
+            z = torch.randn((x.size(0), 128)).to(device)
+            #label = torch.zeros_like(label).to(device)
             y_hat_fake = self.discriminator(self.generator(z))
-            fake_loss = F.cross_entropy(y_hat_fake, label)
+            #fake_loss = F.cross_entropy(y_hat_fake, label)
             
-            loss = real_loss + fake_loss
+            loss = -(y_hat_real - y_hat_fake).mean()
+
+            #loss = real_loss + fake_loss
             
             # logs
             tqdm_dict = {'train_d_loss': loss}
@@ -121,11 +124,10 @@ class GAN(LightningModule):
         elif optimizer_idx == 1:
             x, label = batch
             x, label = x.to(device), label.to(device)
-            label = torch.ones_like(label).to(device)
-            z = torch.rand((x.size(0), 128)).to(device)
-            gen_imgs = self.generator(z)
-            y_hat = self.discriminator(gen_imgs)
-            loss = F.cross_entropy(y_hat, label)
+            z = torch.randn((x.size(0), 128)).to(device)
+            y_hat = self.discriminator(self.generator(z))
+            #loss = F.cross_entropy(y_hat, label)
+            loss = -y_hat.mean()
             
             # logs
             tqdm_dict = {'train_g_loss': loss}
@@ -138,21 +140,27 @@ class GAN(LightningModule):
      
     
     def show_current_generation(self):
-        z = torch.rand((1,128)).to(device)
+        z = torch.randn((1,128)).to(device)
         gen_img = self.generator(z)
-        score = self.discriminator(gen_img)
+        #score = self.discriminator(gen_img)
         
         gen_img = gen_img.squeeze().data.cpu()
-        img = transforms.ToPILImage()(gen_img).convert("RGB")
-        print ("Score of this Generation: ", score)
-        plt.imshow(img)
-        plt.show()
+        #img = transforms.ToPILImage()(gen_img).convert("RGB")
+        #print ("Score of this Generation: ", score)
+        #plt.imshow(img)
+        #plt.show()
+        #self.experiment.add_image("Generated Image {}, img, self.trainer.global_step)
+        return gen_img
 
     def validation_step(self, batch, batch_idx):
         
         # show the first image
         if batch_idx == 0:
-            self.show_current_generation()
+            for i in range(5):
+                img = self.show_current_generation()
+                epoch = self.trainer.current_epoch #global_step
+                step = self.trainer.global_step
+                self.logger.experiment.add_image("Generation {}-{}".format(epoch, i), img, step)
         
         output = self.training_step(batch, batch_idx, 1)
         return {
@@ -165,8 +173,8 @@ class GAN(LightningModule):
         return {'val_loss': avg_loss, 'log': tensorboard_logs}
         
     def configure_optimizers(self):
-        opt_generator = torch.optim.Adam(self.generator.parameters(), lr=1e-3)
-        opt_discriminator = torch.optim.Adam(self.discriminator.parameters(), lr=1e-3)
+        opt_generator = torch.optim.Adam(self.generator.parameters(), lr=1e-4)
+        opt_discriminator = torch.optim.Adam(self.discriminator.parameters(), lr=4e-4)
         return [opt_discriminator, opt_generator]
 
     def train_dataloader(self):
